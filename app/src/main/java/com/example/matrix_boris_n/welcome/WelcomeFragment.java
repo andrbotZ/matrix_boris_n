547ef9db0d6fd5b6c34b8e7bc01508ebcfcf9b21
andrbotx <andrbotx@gmail.com>
2022-07-29 11:54:51 +0300

    welcome fragment added

---
+++ app/src/main/java/com/example/matrix_boris_n/welcome/WelcomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.matrix_boris_n.welcome;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.matrix_boris_n.R;
+import com.example.matrix_boris_n.databinding.FragmentWelcomeBinding;
+
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
